1b3e9fa85073aa955010ffa97023d16389b7c3d7
Ahtazaz Ahmed Khan <49054500+ahmedahtazaz@users.noreply.github.com>
2020-09-28 13:28:16 +0500

    Ingredients added in recipe detail.

--- src/app/recipe/recipe-list/recipe.modal.ts
+++ src/app/recipe/recipe-list/recipe.modal.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from 'src/app/shared/ingredient.modal';
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name: string, desc: string, imagePth: string) {
+    constructor(name: string, desc: string, imagePth: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePth;
+        this.ingredients = ingredients;
     }
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.modal';
 import { Recipe } from './recipe-list/recipe.modal';
 
 @Injectable({
@@ -6,8 +7,8 @@ import { Recipe } from './recipe-list/recipe.modal';
 })
 export class RecipeService {
 
-  private recipes: Recipe[] = [ new Recipe('Test Recipe', 'My Test Recipe', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-                        new Recipe('Another Test Recipe', 'Another My Test Recipe', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')];
+  private recipes: Recipe[] = [ new Recipe('Test Recipe', 'My Test Recipe', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg', [new Ingredient('burger', 5), new Ingredient('banana', 10)]),
+                        new Recipe('Another Test Recipe', 'Another My Test Recipe', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg', [new Ingredient('toffee', 10), new Ingredient('apple', 25)])];
 
   recipeSelected = new EventEmitter<Recipe>();
 
