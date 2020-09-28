4967dc389e84d34eee6b555242c41c44458e5eae
Ahtazaz Ahmed Khan <49054500+ahmedahtazaz@users.noreply.github.com>
2020-09-28 13:19:15 +0500

    Implementation of shoppinglist service.

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.modal';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -11,25 +12,21 @@ export class ShoppingListEditComponent implements OnInit {
   @ViewChild('nameInput') ingredientName: ElementRef;
   @ViewChild('amountInput') ingredientAmount: any;
 
-  @Output('onAddIngredient') addIngredient = new EventEmitter<Ingredient>();
-  @Output('onDeleteIngredient') deleteIngredient = new EventEmitter<any>();
-  @Output('onClearIngredients') clearIngredients = new EventEmitter<any>();
-
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
   onAddingredient = () => {
-    this.addIngredient.emit(new Ingredient(this.ingredientName.nativeElement.value, this.ingredientAmount.nativeElement.value));
+    this.shoppingListService.addIngredient(new Ingredient(this.ingredientName.nativeElement.value, this.ingredientAmount.nativeElement.value));
   }
 
-  onDeleteingredient = () => {
-    this.deleteIngredient.emit();
+  onDeleteingredient = (id: number) => {
+    this.shoppingListService.deleteIngredient(id);
   }
 
   onClearIngredients = () => {
-    this.clearIngredients.emit();
+    this.shoppingListService.clearIngredients();
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.modal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
 
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5), new Ingredient('Tomato', 10)
+  ];
+
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  
   constructor() { }
+
+  getIngredients = () => this.ingredients.slice();
+
+  addIngredient = ingredient => {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.getIngredients());
+  }
+
+  deleteIngredient = (id: number) => {
+    this.ingredients.splice(id, 1);
+  }
+
+  clearIngredients = () => {
+    this.ingredients.length = 0;
+    this.ingredientsChanged.emit(this.getIngredients());
+  }
 }
